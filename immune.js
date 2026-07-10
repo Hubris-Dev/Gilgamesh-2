3e65f3bb8c2e282c2e5b6a8876ffcea646312694
HUBRIS <hubrisdev1@gmail.com>
2026-07-10 12:57:49 -0400

    Update immune.js

--- immune.js
+++ immune.js
@@ -10,7 +10,7 @@
 
 const { sang } = require('../heartbeat');
 const recognition = require('../recognition');
-const filter = require('./filter');
+const filter = require('../filter');
 
 function activate() {
   sang.on('canal:message:recu', (payload = {}) => {
