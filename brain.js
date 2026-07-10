39bd6a03defe86df6a87f0c3732a138e62f2c0cd
HUBRIS <hubrisdev1@gmail.com>
2026-07-10 00:05:14 -0400

    Rename parseEstomac import to parseMessageBrute

--- brain.js
+++ brain.js
@@ -9,7 +9,7 @@ const path = require('path');
 const { sang } = require('./core/heartbeat');
 const { resolveKryvenPulse, resolveGroqPulse } = require('./core/kryven-client');
 const { getMemory, appendMemory } = require('./memory/mongo');
-const { parseEstomac } = require('./utils/parser');
+const { parseMessageBrute: parseEstomac } = require('./utils/parser');
 const {  isSafeInput } = require('./security/filter');
 
 // Charger l'identité — l'ADN du Nerf (Système Endocrinien)
